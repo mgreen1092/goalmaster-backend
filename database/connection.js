ef4496ee34c668207340ce4b7594b9246cbff26a
mgreen1092 <morgan.green0209@gmail.com>
2022-12-23 15:37:30 -0500

    .env and database url added

---
+++ database/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.Promise = Promise
+
+const mongoURI = process.env.DATABASE_URL
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((instance) =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch((error) => console.log('Connection failed!', error))
+
+module.exports = mongoose;
