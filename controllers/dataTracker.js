cf88b64cb88b42361dd440bc7af41e47c68665f1
mgreen1092 <morgan.green0209@gmail.com>
2022-12-27 11:30:26 -0500

    updated data tracker routes with additional routes

--- controllers/dataTracker.js
+++ controllers/dataTracker.js
@@ -10,4 +10,21 @@ router.get('/', async (req, res, next) => {
         next(err)
     }
 })
+router.get('/:id', async (req, res, next) => {
+    try {
+        const data = await DataPoints.findById(req.params.id)
+        data ? res.json(data) : res.status(404)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newDataPoint = await DataPoints.create(req.body)
+        newDataPoint ? res.status(201).json(newDataPoint) : res.status(422) // 422 (Unprocessable Entity) - missing a required field
+    } catch (err) {
+        next(err)
+    }
+})
 module.exports = router
