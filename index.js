2034ffe46ecfa8de3c1af181d7c4d874643edd71
mgreen1092 <morgan.green0209@gmail.com>
2022-12-27 10:02:49 -0500

    CORS added

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const userController = require('./controllers/user')
 const goalController = require('./controllers/goal')
-const trackerController = require('./controllers/dataTracker')
+const dataController = require('./controllers/dataTracker')
 // const firebaseAuth = require('./authMiddleware')
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use(firebaseAuth.authenticate)
 
 app.get('/', (req, res) => {
     res.redirect('/api/users')
@@ -12,6 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/users/', userController)
 app.use('/api/goals/', goalController)
+app.use('/dataController', dataController)
 
 app.set('port', process.env.PORT || 8080)
 
