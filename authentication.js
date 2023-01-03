aaa042272b76d5398eda93b75fe2259d8c392f16
mgreen1092 <morgan.green0209@gmail.com>
2023-01-03 15:42:50 -0500

    firebaseConfig and authenitcation set up

---
+++ authentication.js
@@ -0,0 +1,26 @@
+const admin = require('./database/firebaseConfig')
+
+class FirebaseAuth {
+    async authenticate(req, res, next) {
+        let token
+        if (req.headers.authorization) {
+            console.log(req.headers)
+            token = req.headers.authorization.split(' ')[1]
+        } else {
+            return res.status(401).json({ message: 'No authentication provided' })
+        }
+        try {
+            const decodeValue = await admin.auth().verifyIdToken(token);
+            if (decodeValue) {
+                return next()
+            }
+            return res.status(401).json({ message: 'Unauthorized' })
+        } catch (err) {
+            return err.code === 'auth/argument-error'
+                ? res.status(401).json({ message: err.message })
+                : res.status(500).json({ message: 'Internal Server Error' })
+        }
+    }
+}
+
+module.exports = new FirebaseAuth()
